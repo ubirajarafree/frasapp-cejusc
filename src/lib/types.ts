7612c35d04e37acdc82f8b4df23c519e8d172eea
uabj8000 <uabj8000@gmail.com>
2025-10-09 20:56:54 -0300

    update types ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,17 @@
+export enum Categoria {
+  Reflexao = "Reflexão",
+  Motivacao = "Motivação",
+  Humor = "Humor",
+  Espiritualidade = "Espiritualidade",
+  Amor = "Amor",
+  Gratidao = "Gratidão",
+}
+
 export type Frase = {
   id: number;
   titulo: string | null;
   conteudo: string;
   autor: string | null;
+  categoria: Categoria;
+  created_at: string;
 };
